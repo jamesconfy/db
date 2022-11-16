567b31e69b55b1bc67afa2965fc4ced668d2e80d
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-11-16 18:36:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 		panic(err1.Error())
 	}
 
+	// Get environment variables
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_HOST := os.Getenv("DB_HOST")
@@ -26,21 +27,18 @@ func main() {
 	DB_DATABASE := os.Getenv("DB_DATABASE")
 
 	// Create a connection to the database
-	database_uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
-	db, err := sql.Open("mysql", database_uri)
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// db.Exec(db.sql)
-
 	// Ping the database to make sure the connection is still active
-	// connect := db.Ping()
-	// if connect != nil {
-	// 	fmt.Println("Could not connect to database")
-	// 	panic(connect.Error())
-	// }
+	connect := db.Ping()
+	if connect != nil {
+		fmt.Println("Could not connect to database")
+		panic(connect.Error())
+	}
 
 	// Select data from the database
 	users, err := db.Query(`SELECT * from Users`)
@@ -50,7 +48,7 @@ func main() {
 	}
 
 	fmt.Println(users)
-	// defer users.Close()
+	defer users.Close()
 
 	fmt.Println("Connection successful")
 }
